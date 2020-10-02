ef5c85da822b2d03ab07145d8a899646daf1917d
ClaySamuelC <claysamuelc@gmail.com>
2020-10-01 20:34:35 -0700

    add server components

--- server/index.js
+++ server/index.js
@@ -1,28 +1,9 @@
 const bodyParser = require('body-parser');
 const express = require('express');
-const mongoose = require('mongoose');
-
-const app = express();
+const app = require('./app');
 
 // Port 3003 corresponds to Photo Carousel Module from the original server
 const port = 3003;
-
-// options
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// base directory
-app.use(express.static(__dirname + '/../client'));
-
-app.get('/carousel', (req, res) => {
-  let carousel = Carousel.find({houseName: req.houseName});
-  res.send(carousel);
-});
-
-app.post('/post', (req, res) => {
-  Carousel.create({houseId: req.body.houseId, houseName: req.body.houseName, album: req.body.album});
-  res.end();
-});
 
 app.listen(port, () => {
   console.log('server started on http://localhost:' + port);
