e91867be283bb468086ed3dce09ea324896e0a0d
claysamuelc <claysamuelc@gmail.com>
2020-10-07 18:40:45 -0500

    udpate webpack config and server client call to properly render the file

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // base directory
-app.use(express.static(path.join(__dirname, '/..client/dist')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('/test', (req, res) => {
   res.status(200).send('get request received');
