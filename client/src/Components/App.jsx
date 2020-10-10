0ef89558a1e473a137954d89ccd5d8f4a5b1e142
claysamuelc <claysamuelc@gmail.com>
2020-10-10 12:24:42 -0500

    update css to be compatable with proxy server

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -23,24 +23,28 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div id="sam">
         <div class="image-carousel">
           <div class="fullscreen-button"/>
           <div class="PhotoArea">
             <div class="nav prev" onClick={e => this.moveLeft()}>
               <div class="navButton"/>
             </div>
+
             <div class="nav next" onClick={e => this.moveRight()}>
               <div class="navButton"/>
             </div>
+
             <div id="photoTracker">
               {this.state.currentImage + ' of ' + this.state.images.length}
             </div>
+
             <div class="image-full">
               <img src={this.state.images[this.state.currentImage - 1]} id="imageUrl"/>
             </div>
           </div>
         </div>
+
         <div class="bottom-banner">
           <div class="card" id="album">
             <div class="cardv2">
@@ -50,22 +54,25 @@ class App extends React.Component {
               </div>
             </div>
           </div>
+
           <div class="card" id="3d-walkthrough">
             <div class="cardv2">
-              <img class="card-image" src="https://cdn.aarp.net/content/dam/aarp/home-and-family/your-home/2018/06/1140-house-inheriting.imgcache.rev68c065601779c5d76b913cf9ec3a977e.jpg"/>
+              <img class="card-image" src="https://mightyvisage.co.uk/wp-content/uploads/2019/04/animation-play-button.jpg"/>
               <div class="center">
                 <span class="bottom-word">3D Walkthrough</span>
               </div>
             </div>
           </div>
+
           <div class="card" id="street-view">
             <div class="cardv2">
-              <img class="card-image" src="https://cdnimages.familyhomeplans.com/plans/51997/51997-b600.jpg"/>
+              <img class="card-image" src="https://cdn.images.express.co.uk/img/dynamic/25/590x/Google-Maps-Street-View-Wayne-s-World-961777.jpg"/>
               <div class="center">
                 <span class="bottom-word">Street View</span>
               </div>
             </div>
           </div>
+
         </div>
       </div>
     )
